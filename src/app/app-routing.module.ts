390136be253847c4979a10d9d77914dae2f3cfa2
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2019-08-11 23:44:55 +0530

    Fixing some issues between the two branches!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { EventsComponent } from './events/events.component';
+import { AboutComponent } from './about/about.component';
+import { MembersComponent } from './members/members.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomepageComponent}
+  { path: '', component: HomepageComponent },
+  { path: 'events', component: EventsComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'members', component: MembersComponent},
+  { path: 'contact', component: ContactComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { ConsoleComponent } from './console/console.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { EventsComponent } from './events/events.component';
+import { MembersComponent } from './members/members.component';
+import { ShellComponent } from './shell/shell.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    ConsoleComponent
+    AboutComponent,
+    ContactComponent,
+    EventsComponent,
+    MembersComponent,
+    ShellComponent
   ],
   imports: [
     BrowserModule,
